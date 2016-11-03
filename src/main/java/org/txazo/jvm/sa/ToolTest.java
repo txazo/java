2b7ea79e37ee6c72dc35c7b26556b7197aee5fe7
txazo <784990655@qq.com>
2016-11-04 00:20:26 +0800

    update

--- src/main/java/org/txazo/jvm/sa/ToolTest.java
+++ src/main/java/org/txazo/jvm/sa/ToolTest.java
@@ -1,13 +1,11 @@
 package org.txazo.jvm.sa;
 
-import java.io.IOException;
-
 public class ToolTest {
 
     /**
      * VM Args: -server -Xms40M -Xmx40M -XX:+UseParallelGC -XX:OnOutOfMemoryError="sudo java -cp .:$JAVA_HOME/lib/sa-jdi.jar:/Users/txazo/TxazoProject/java/target/java-1.0.jar org.txazo.jvm.sa.PsHeapPrintTest %p > /Users/txazo/test/outofmemoryerror.log" -Dfile.encoding=UTF-8
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Byte[] longs = new Byte[40 * 1024 * 1024];
     }
 
