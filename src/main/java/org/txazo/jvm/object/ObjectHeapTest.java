227437a7d17164089a81d9e0d9e769f3e5883239
txazo <784990655@qq.com>
2016-11-12 22:12:02 +0800

    update

---
+++ src/main/java/org/txazo/jvm/object/ObjectHeapTest.java
@@ -0,0 +1,36 @@
+package org.txazo.jvm.object;
+
+import sun.jvm.hotspot.oops.*;
+import sun.jvm.hotspot.runtime.VM;
+import sun.jvm.hotspot.tools.Tool;
+import sun.jvm.hotspot.utilities.SystemDictionaryHelper;
+
+public class ObjectHeapTest extends Tool {
+
+    @Override
+    public void run() {
+        VM vm = VM.getVM();
+        ObjectHeap objectHeap = vm.getObjectHeap();
+        HeapVisitor heapVisitor = new HeapPrinter(System.out);
+        InstanceKlass klass = SystemDictionaryHelper.findInstanceKlass("Debugger$AAA");
+        objectHeap.iterateObjectsOfKlass(heapVisitor, klass, false);
+        objectHeap.iterate(heapVisitor, new ObjectHeap.ObjectFilter() {
+
+            @Override
+            public boolean canInclude(Oop oop) {
+                return false;
+            }
+
+        });
+    }
+
+    /**
+     * sudo java -cp .:$JAVA_HOME/lib/sa-jdi.jar:java-1.0.jar org.txazo.jvm.object.ObjectHeapTest pid
+     */
+    public static void main(String[] args) {
+        ObjectHeapTest test = new ObjectHeapTest();
+        test.start(args);
+        test.stop();
+    }
+
+}
