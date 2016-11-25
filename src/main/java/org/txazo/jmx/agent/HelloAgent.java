15653f30e8e46c8b6aa4a8fb6dc8e508f0776305
txazo <784990655@qq.com>
2016-11-25 18:23:15 +0800

    update

--- src/main/java/org/txazo/jmx/agent/HelloAgent.java
+++ src/main/java/org/txazo/jmx/agent/HelloAgent.java
@@ -1,10 +1,12 @@
 package org.txazo.jmx.agent;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.txazo.jmx.mbean.standard.Hello;
+import org.txazo.jmx.mbean.standard.HelloMBean;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
+import javax.management.*;
 import java.lang.management.ManagementFactory;
+import java.util.Arrays;
 
 public class HelloAgent {
 
@@ -13,11 +15,73 @@ public class HelloAgent {
      */
     public static void main(String[] args) throws Exception {
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName objectName = new ObjectName("org.txazo.jmx.mbean.standard:type=Hello");
-        Hello hello = new Hello(1, "hello", 25);
-        mbs.registerMBean(hello, objectName);
-        System.out.println("mbean registered ...");
+        registerMBean(mbs);
+        createMBean(mbs);
+        printMBeanInfo(mbs.getMBeanInfo(ObjectName.getInstance("java.lang:type=Memory")));
+        printMBeanInfo(mbs.getMBeanInfo(ObjectName.getInstance("java.lang:type=Threading")));
         System.in.read();
+    }
+
+    private static void registerMBean(MBeanServer mbs) throws Exception {
+        ObjectName name = ObjectName.getInstance("org.txazo.jmx.mbean.standard:type=Hello,name=local1");
+        HelloMBean bean = new Hello(1, "local", 25);
+        mbs.registerMBean(bean, name);
+    }
+
+    private static void createMBean(MBeanServer mbs) throws Exception {
+        String className = "org.txazo.jmx.mbean.standard.Hello";
+        ObjectName name = ObjectName.getInstance("org.txazo.jmx.mbean.standard:type=Hello,name=local2");
+        Object[] params = new Object[]{1, "local", 25};
+        String[] signatures = new String[]{"int", "java.lang.String", "int"};
+        mbs.createMBean(className, name, params, signatures);
+    }
+
+    private static void printMBeanInfo(MBeanInfo mBeanInfo) {
+        mBeanInfo.getClassName();
+        MBeanConstructorInfo[] constructorInfos = mBeanInfo.getConstructors();
+        if (ArrayUtils.isNotEmpty(constructorInfos)) {
+            for (MBeanConstructorInfo info : constructorInfos) {
+                System.out.println("Constructor: " + info.getName() + buildParameters(info.getSignature()));
+            }
+        }
+
+        MBeanAttributeInfo[] attributeInfos = mBeanInfo.getAttributes();
+        if (ArrayUtils.isNotEmpty(attributeInfos)) {
+            for (MBeanAttributeInfo info : attributeInfos) {
+                System.out.println("Attribute: [name=" + info.getName() + ", type=" + info.getType() + ", read=" + info.isReadable() + ", write=" + info.isWritable() + "]");
+            }
+        }
+
+        MBeanOperationInfo[] operationInfos = mBeanInfo.getOperations();
+        if (ArrayUtils.isNotEmpty(operationInfos)) {
+            for (MBeanOperationInfo info : operationInfos) {
+                System.out.println("Operation: " + info.getReturnType() + " " + info.getName() + buildParameters(info.getSignature()));
+            }
+        }
+
+        MBeanNotificationInfo[] notificationInfos = mBeanInfo.getNotifications();
+        if (ArrayUtils.isNotEmpty(notificationInfos)) {
+            for (MBeanNotificationInfo info : notificationInfos) {
+                System.out.println("Notification: " + info.getName() + " " + Arrays.toString(info.getNotifTypes()));
+            }
+        }
+    }
+
+    private static String buildParameters(MBeanParameterInfo[] parameterInfos) {
+        if (ArrayUtils.isEmpty(parameterInfos)) {
+            return "()";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        for (int i = 0; i < parameterInfos.length; i++) {
+            if (i == 0) {
+                sb.append(parameterInfos[i].getType());
+            } else {
+                sb.append(", ").append(parameterInfos[i].getType());
+            }
+        }
+        sb.append(")");
+        return sb.toString();
     }
 
 }
--- src/main/java/org/txazo/jmx/client/JMXClient.java
+++ src/main/java/org/txazo/jmx/client/JMXClient.java
@@ -1,5 +1,8 @@
 package org.txazo.jmx.client;
 
+import org.txazo.jmx.mbean.standard.HelloMBean;
+
+import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectInstance;
 import javax.management.ObjectName;
@@ -14,16 +17,39 @@ public class JMXClient {
         JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://:9999/jmxrmi");
         JMXConnector jmxc = JMXConnectorFactory.connect(url);
         MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
-        mbsc.getMBeanCount();
-        Set<ObjectName> objectNames = mbsc.queryNames(null, null);
-        for (ObjectName objectName : objectNames) {
-            System.out.println(objectName.getCanonicalName());
+
+        // Domain列表
+        String[] domains = mbsc.getDomains();
+        for (int i = 0; i < domains.length; i++) {
+            System.out.println("Domain[" + i + "]: " + domains[i]);
         }
 
-        Set<ObjectInstance> objectInstances = mbsc.queryMBeans(null, null);
-        for (ObjectInstance objectInstance : objectInstances) {
-            System.out.println(objectInstance.getClassName());
+        // 默认Domain
+        System.out.println("Default Domain: " + mbsc.getDefaultDomain());
+
+        // 创建MBean
+        mbsc.createMBean("org.txazo.jmx.mbean.standard.Hello", ObjectName.getInstance("org.txazo.jmx.mbean.standard:type=Hello,name=remote"), new Object[]{2, "remote", 30}, new String[]{"int", "java.lang.String", "int"});
+
+        // MBean数量
+        System.out.println("MBean count: " + mbsc.getMBeanCount());
+
+        // ObjectName列表
+        Set<ObjectName> objectNames = mbsc.queryNames(null, null);
+        for (ObjectName name : objectNames) {
+            System.out.println("ObjectName: " + name.getCanonicalName());
         }
+
+        // ObjectInstance列表
+        Set<ObjectInstance> objectInstances = mbsc.queryMBeans(null, null);
+        for (ObjectInstance instance : objectInstances) {
+            System.out.println("ObjectInstance: " + instance.getObjectName().getCanonicalName() + ", " + instance.getClassName());
+        }
+
+        // 创建MBean代理
+        HelloMBean helloProxy = JMX.newMBeanProxy(mbsc, ObjectName.getInstance("org.txazo.jmx.mbean.standard:type=Hello,name=local"), HelloMBean.class);
+        System.out.println("Hello: id=" + helloProxy.getId() + ", name=" + helloProxy.getName());
+        helloProxy.setAge(45);
+        helloProxy.operation();
 
         jmxc.close();
     }
