bd1469c09b37782d646f8a4ba70435dfaffeede4
txazo <784990655@qq.com>
2017-06-07 00:10:19 +0800

    update

---
+++ src/main/java/org/txazo/hadoop/wordcount/WordCount.java
@@ -0,0 +1,66 @@
+package org.txazo.hadoop.wordcount;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+
+public class WordCount {
+
+    private static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] words = line.split("\\s");
+            for (String word : words) {
+                context.write(new Text(word), new IntWritable(1));
+            }
+        }
+
+    }
+
+    private static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable count : values) {
+                sum += count.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: WordCount <input> <output>");
+            System.exit(-1);
+        }
+
+        Job job = Job.getInstance(conf, "WordCount");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
