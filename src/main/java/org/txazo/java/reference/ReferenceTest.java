b0491d04c19cbb8c0d1290d509c63745b25d7a5a
txazo <784990655@qq.com>
2017-09-24 23:43:52 +0800

    update

--- src/main/java/org/txazo/java/reference/ReferenceTest.java
+++ src/main/java/org/txazo/java/reference/ReferenceTest.java
@@ -3,13 +3,23 @@ package org.txazo.java.reference;
 /**
  * 引用
  *
+ * 引用对象封装了对其它对象的引用
+ *
  * 子类
  * ------------------------------------------------------------
  * - SoftReference          软引用
  * - WeakReference          弱引用
+ * - PhantomReference       虚引用
+ * -
+ * - 引用级别: 强引用 > 软引用 > 弱引用 > 虚引用
+ * - 强引用    被非Reference对象引用
+ * - 软引用    非强引用, 被SoftReference对象引用
+ * - 弱引用    非强引用、软引用, 被WeakReference对象引用
+ * - 虚引用    非强引用、软引用、虚引用, 被PhantomReference对象引用
+ *
+ * - 下面两种在jdk内部使用
  * - FinalReference         Final引用
  * -    Finalizer
- * - PhantomReference       虚引用
  * - Cleaner                Cleaner
  * ------------------------------------------------------------
  *
