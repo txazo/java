5eca955dea43432c5b2323dc34c1616ae3f495b4
txazo <784990655@qq.com>
2017-09-24 13:39:13 +0800

    update

--- src/main/java/org/txazo/java/reference/ReferenceTest.java
+++ src/main/java/org/txazo/java/reference/ReferenceTest.java
@@ -4,14 +4,16 @@ package org.txazo.java.reference;
  * 引用
  *
  * 子类
- * --------------------------------------------------
+ * ------------------------------------------------------------
  * - SoftReference          软引用
  * - WeakReference          弱引用
  * - FinalReference         Final引用
  * -    Finalizer
  * - PhantomReference       虚引用
  * - Cleaner                Cleaner
- * --------------------------------------------------
+ * ------------------------------------------------------------
+ *
+ * Reference的构造函数是package的, 不能在用户代码中被继承, 只能继承它的子类
  *
  * @see java.lang.ref.Reference
  * @see java.lang.ref.Reference.ReferenceHandler
